c81aca54ef7503f48cfa8096a48f0a55f679fe6b
Emmanuel Ngwane <ngwanem@hotmail.com>
2014-11-25 10:21:04 +0100

    Initial creation

---
+++ solution/xinterop.infrastructure.concretes/xinterop.infrastructure.concretes.cpp
@@ -0,0 +1,26 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "xinterop.infrastructure.concretes.h"
+
+//Implementation of reexmonkey.xinterop.infrastructure.concretes.InteropExtensions
+
+namespace xs = reexmonkey::xinterop::infrastructure::concrete;
+namespace interop = System::Runtime::InteropServices;
+
+const std::string xs::InteropExtensions::ToNativeString(String^ s)
+{
+	auto str = (const char*)(interop::Marshal::StringToHGlobalAnsi(s).ToPointer());
+	//create std::string from str;
+	std::string converted(str);
+	//free resources
+	interop::Marshal::FreeHGlobal(IntPtr((void*)str));
+	return converted;
+}
+
+
+String^ xs::InteropExtensions::ToManagedString(const std::string& s)
+{
+	return gcnew String(s.c_str());
+}
---
+++ solution/xinterop.infrastructure.concretes/xinterop.infrastructure.concretes.h
@@ -0,0 +1,38 @@
+// xinterop.infrastructure.concretes.h
+
+#pragma once
+
+//unamanaged references
+#include <string>
+
+using namespace System;
+
+namespace reexmonkey
+{
+	namespace xinterop {
+		namespace infrastructure{
+			namespace concrete{
+				/// <summary>
+				/// Helper class to provide interop conversion properties
+				/// </summary>
+				public ref class InteropExtensions abstract sealed
+				{
+				public:
+					/// <summary>
+					/// Converts a managed string to corresponding native (STL) string.
+					/// </summary>
+					/// <param name="s">The managed string to convert</param>
+					/// <returns>The native string equivalent</returns>
+					static const std::string ToNativeString(String^ s);
+
+					/// <summary>
+					/// Converts a native (STL) string to corresponding managed string.
+					/// </summary>
+					/// <param name="s">The native (STL) string to convert</param>
+					/// <returns>The managed string equivalent</returns>
+					static String^ ToManagedString(const std::string& s);
+				};
+			}
+		}
+	}
+}
