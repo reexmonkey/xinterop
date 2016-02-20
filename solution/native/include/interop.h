4e46610fd203b6357f66c955c7a5a1c33d5c9e77
Emmanuel Ngwane <ngwanemk@gmail.com>
2016-02-20 16:13:35 +0100

    Fixed building against codecvt  for various platform toolsets in vs 2015. Added configuration file for nuget package.

--- solution/native/include/interop.h
+++ solution/native/include/interop.h
@@ -8,6 +8,17 @@
 using namespace System;
 using namespace System::Runtime::InteropServices;
 
+#if _MSC_PLATFORM_TOOLSET == 140
+
+namespace std
+{
+	typedef std::basic_string<unsigned short, std::char_traits<unsigned short>, std::allocator<unsigned short> > utf16string;
+	typedef std::basic_string<unsigned int, std::char_traits<unsigned int>, std::allocator<unsigned int> > utf32string;
+
+}
+#endif
+
+
 namespace reexmonkey
 {
 	namespace xinterop 
@@ -15,7 +26,7 @@ namespace reexmonkey
 		namespace services
 		{
 			/// <summary>
-			/// Helper class to provide interop conversion properties
+			/// Service class to provide conversion features between managed and native types.
 			/// </summary>
 			public ref class InteropService abstract sealed
 			{
@@ -26,61 +37,210 @@ namespace reexmonkey
 				/// This method indicates to the garbage collector not to move the referenced mangaed string during clean up operations.
 				/// </summary>
 				/// <param name="s">The managed string to be converted.</param>
-				/// <returns>The resulting native string equivalent</returns>
+				/// <returns>The equivalent native string from the conversion.</returns>
 				static const std::string ToNativeAnsiString(String^ s);
 
 				/// <summary>
 				/// Converts a native ANSI string to corresponding managed string.
-				/// Use this method when you want to pass a native string to a managed function or string. 
+				/// Use this method when you want to pass a native string to a managed function or managed string. 
 				/// </summary>
-				/// <param name="s">The native (STL) string to be converted</param>
-				/// <returns>The resulting managed string equivalent</returns>
+				/// <param name="s">The native string to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
 				static String^ FromNativeAnsiString(const std::string& s);
 
 				/// <summary>
-				/// Converts a managed UTF-16 string to a corresponding native Unicode string.
+				/// Converts a managed UTF-16 string to a corresponding native wide character string.
 				/// Use this method when you wish to pass a managed string safely to a native function or string.
-				/// This method indicates to the garbage collector not to move the referenced mangaed string during clean up operations.
+				/// This method also indicates to the garbage collector not to move the referenced managed string reference during clean up operations.
 				/// </summary>
 				/// <param name="s"></param>
-				/// <returns></returns>
-				static const std::wstring ToNativeUnicodeString(String^ s);
+				/// <returns>The equivalent native wide character string from the conversion.</returns>
+				static const std::wstring ToNativeWideString(String^ s);
 
 				/// <summary>
-				/// 
+				/// Converts a native wide-character string to a managed UTF-16 string.
+				/// Use this method when you want to pass a native wide-character string to a managed function or managed string. 
 				/// </summary>
-				/// <param name="s"></param>
-				/// <returns></returns>
-				static String^ FromNativeUnicodeString(const std::wstring& s);
+				/// <param name="s">The wide-character string to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion</returns>
+				static String^ FromNativeWideString(const std::wstring& s);
 
 				/// <summary>
-				/// 
+				///  Converts a managed UTF-16 string to a native fixed array of ANSI characters.
+				///  Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of ANSI characters.
 				/// </summary>
-				/// <param name="s"></param>
-				/// <returns></returns>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of ANSI characters.</returns>
 				static std::vector<char> ToNativeAnsiChars(String^ s);
 
 				/// <summary>
-				/// 
+				/// Converts a native fixed array of ANSI characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native fixed array of ANSI characters to a managed function or managed string. 
 				/// </summary>
-				/// <param name="chars"></param>
-				/// <returns></returns>
+				/// <param name="chars">The native fixed array of ANSI characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
 				static String^ FromNativeAnsiChars(std::vector<char> chars);
 
 
 				/// <summary>
-				/// 
+				/// Converts a managed UTF-16 string to a native fixed array of wide characters.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of native wide characters.
 				/// </summary>
-				/// <param name="s"></param>
-				/// <returns></returns>
-				static std::vector<wchar_t> ToNativeUnicodeChars(String^ s);
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of wide characters.</returns>
+				static std::vector<wchar_t> ToNativeWideChars(String^ s);
 
 				/// <summary>
-				/// 
+				/// Converts a native fixed array of wide characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native fixed array of wide characters to a managed function or managed string. 
 				/// </summary>
-				/// <param name="wchars"></param>
-				/// <returns></returns>
-				static String^ FromNativeUnicodeChars(std::vector<wchar_t> wchars);
+				/// <param name="wchars">The native fixed array of wide characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeWideChars(std::vector<wchar_t> wchars);
+
+				#if _MSC_PLATFORM_TOOLSET == 140
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native UTF-16 string.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function or native UTF-16 string.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent native native UTF-16 string.</returns>
+				static std::utf16string ToNativeUtf16String(String^ s);
+
+				/// <summary>
+				/// Converts a native UTF-16 string to a managed UTF-16 string.
+				/// Use this method when you want to pass a native UTF-16 string to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf16">The native UTF-16 string to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeUtf16String(std::utf16string utf16);
+
+
+				/// <summary>
+				/// Converts a managed UTF-32 string to a native UTF-32 string.
+				/// Use this method, when you want to pass a managed UTF-32 string to a native function or native UTF-32 string.
+				/// </summary>
+				/// <param name="s">The managed UTF-32 string to be converted.</param>
+				/// <returns>The equivalent native native UTF-32 string.</returns>
+				static std::utf32string ToNativeUtf32String(String^ s);
+
+
+				/// <summary>
+				/// Converts a native UTF-32 string to a managed UTF-32 string.
+				/// Use this method when you want to pass a native UTF-32 string to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf32">The native UTF-32 string to be converted.</param>
+				/// <returns>The equivalent managed UTF-32 string from the conversion.</returns>
+				static String^ FromNativeUtf32String(std::utf32string utf32);
+
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native fixed array of native UTF-16 characters.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of native UTF-16 characters.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of native UTF-16 characters.</returns>
+				static std::vector<unsigned short> ToNativeUtf16Chars(String^ s);
+
+				/// <summary>
+				/// Converts a native fixed array of native UTF-32 characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native array of UTF-16 characters to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf16">The native fixed array of native UTF-16 characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeUtf16Chars(std::vector<unsigned short> utf16);
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native fixed array of native UTF-32 characters.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of native UTF-32 characters.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of native UTF-32 characters.</returns>
+				static std::vector<unsigned int> ToNativeUtf32Chars(String^ s);
+
+				/// <summary>
+				/// Converts a native fixed array of native UTF-32 characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native array of UTF-32 characters to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf32">The native fixed array of native UTF-32 characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-32 string from the conversion.</returns>
+				static String^ FromNativeUtf32Chars(std::vector<unsigned int> utf32);
+
+				#else
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native UTF-16 string.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function or native UTF-16 string.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent native native UTF-16 string.</returns>
+				static std::u16string ToNativeUtf16String(String^ s);
+
+				/// <summary>
+				/// Converts a native UTF-16 string to a managed UTF-16 string.
+				/// Use this method when you want to pass a native UTF-16 string to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf16">The native UTF-16 string to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeUtf16String(std::u16string utf16);
+
+
+				/// <summary>
+				/// Converts a managed UTF-32 string to a native UTF-32 string.
+				/// Use this method, when you want to pass a managed UTF-32 string to a native function or native UTF-32 string.
+				/// </summary>
+				/// <param name="s">The managed UTF-32 string to be converted.</param>
+				/// <returns>The equivalent native native UTF-32 string.</returns>
+				static std::u32string ToNativeUtf32String(String^ s);
+
+
+				/// <summary>
+				/// Converts a native UTF-32 string to a managed UTF-32 string.
+				/// Use this method when you want to pass a native UTF-32 string to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf32">The native UTF-32 string to be converted.</param>
+				/// <returns>The equivalent managed UTF-32 string from the conversion.</returns>
+				static String^ FromNativeUtf32String(std::u32string utf32);
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native fixed array of native UTF-16 characters.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of native UTF-16 characters.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of native UTF-16 characters.</returns>
+				static std::vector<char16_t> ToNativeUtf16Chars(String^ s);
+
+
+				/// <summary>
+				/// Converts a native fixed array of native UTF-32 characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native array of UTF-16 characters to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf16">The native fixed array of native UTF-16 characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeUtf16Chars(std::vector<char16_t> utf16);
+
+
+
+				/// <summary>
+				/// Converts a managed UTF-16 string to a native fixed array of native UTF-32 characters.
+				/// Use this method, when you want to pass a managed UTF-16 string to a native function that requires a native fixed array of native UTF-32 characters.
+				/// </summary>
+				/// <param name="s">The managed UTF-16 string to be converted.</param>
+				/// <returns>The equivalent fixed array of native UTF-32 characters.</returns>
+				static std::vector<char32_t> ToNativeUtf32Chars(String^ s);
+
+
+				/// <summary>
+				/// Converts a native fixed array of native UTF-32 characters to a managed UTF-16 string.
+				/// Use this method when you want to pass a native array of UTF-16 characters to a managed function or managed string. 
+				/// </summary>
+				/// <param name="utf32">The native fixed array of native UTF-16 characters to be converted.</param>
+				/// <returns>The equivalent managed UTF-16 string from the conversion.</returns>
+				static String^ FromNativeUtf32Chars(std::vector<char32_t> utf32);
+
+				#endif 
+
 
 
 				/// <summary>
--- solution/native/src/interop.cpp
+++ solution/native/src/interop.cpp
@@ -1,6 +1,7 @@
 
 #include "stdafx.h"
 #include <vcclr.h>
+#include <codecvt>
 #include "interop.h"
 
 
@@ -20,7 +21,7 @@ const std::string InteropService::ToNativeAnsiString(String ^ s)
 	return converted;
 }
 
-const std::wstring InteropService::ToNativeUnicodeString(String ^ s)
+const std::wstring InteropService::ToNativeWideString(String ^ s)
 {
 	pin_ptr<const wchar_t> str = PtrToStringChars(s);
 	std::wstring ws(str);
@@ -45,7 +46,7 @@ String ^ InteropService::FromNativeAnsiChars(std::vector<char> chars)
 	return gcnew String(s.c_str());
 }
 
-std::vector<wchar_t> InteropService::ToNativeUnicodeChars(String ^ s)
+std::vector<wchar_t> InteropService::ToNativeWideChars(String^ s)
 {
 	pin_ptr<const wchar_t> str = PtrToStringChars(s);
 	std::wstring ws(str);
@@ -54,18 +55,200 @@ std::vector<wchar_t> InteropService::ToNativeUnicodeChars(String ^ s)
 	return writable;
 }
 
-String ^ InteropService::FromNativeUnicodeChars(std::vector<wchar_t> wchars)
+String ^ InteropService::FromNativeWideChars(std::vector<wchar_t> wchars)
 {
 	std::wstring w(wchars.begin(), wchars.end());
 	return gcnew String(w.c_str());
 }
+
+
+
+#if _MSC_PLATFORM_TOOLSET == 140
+
+std::utf16string InteropService::ToNativeUtf16String(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
+	std::utf16string u16string = convert.from_bytes(utf8);
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return u16string;
+
+}
+
+String ^ InteropService::FromNativeUtf16String(std::utf16string utf16)
+{
+	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
+	std::string utf8 = convert.to_bytes(utf16);
+	return gcnew String(utf8.c_str());
+}
+
+std::utf32string InteropService::ToNativeUtf32String(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8<uint32_t>, uint32_t> convert;
+	std::utf32string utf32 = convert.from_bytes(utf8);
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return utf32;
+}
+
+String ^ InteropService::FromNativeUtf32String(std::utf32string utf32)
+{
+	std::wstring_convert<std::codecvt_utf8_utf16<uint32_t>, uint32_t> convert;
+	std::string utf8 = convert.to_bytes(utf32);
+	return gcnew String(utf8.c_str());
+}
+
+std::vector<uint16_t> InteropService::ToNativeUtf16Chars(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
+	std::utf16string u16string = convert.from_bytes(utf8);
+	std::vector<uint16_t> writable(u16string.begin(), u16string.end());
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return writable;
+}
+
+String ^ InteropService::FromNativeUtf16Chars(std::vector<uint16_t> utf16)
+{
+	std::wstring w(utf16.begin(), utf16.end());
+	return gcnew String(w.c_str());
+}
+
+std::vector<uint32_t> InteropService::ToNativeUtf32Chars(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<uint32_t>, uint32_t> convert;
+	std::utf32string utf32 = convert.from_bytes(utf8);
+	std::vector<uint32_t> writable(utf32.begin(), utf32.end());
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return writable;
+}
+
+String ^ InteropService::FromNativeUtf32Chars(std::vector<uint32_t> utf32)
+{
+	std::wstring w(utf32.begin(), utf32.end());
+	return gcnew String(w.c_str());
+}
+
+#else
+
+std::u16string InteropService::ToNativeUtf16String(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	std::u16string u16string = convert.from_bytes(utf8);
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return u16string;
+
+}
+
+String ^ InteropService::FromNativeUtf16String(std::u16string utf16)
+{
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	std::string utf8 = convert.to_bytes(utf16);
+	return gcnew String(utf8.c_str());
+}
+
+std::u32string InteropService::ToNativeUtf32String(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+	std::u32string utf32 = convert.from_bytes(utf8);
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return utf32;
+}
+
+String ^ InteropService::FromNativeUtf32String(std::u32string utf32)
+{
+	std::wstring_convert<std::codecvt_utf8_utf16<char32_t>, char32_t> convert;
+	std::string utf8 = convert.to_bytes(utf32);
+	return gcnew String(utf8.c_str());
+}
+
+std::vector<char16_t> InteropService::ToNativeUtf16Chars(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	std::u16string u16string = convert.from_bytes(utf8);
+
+	std::vector<char16_t> writable(u16string.begin(), u16string.end());
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return writable;
+}
+
+String ^ InteropService::FromNativeUtf16Chars(std::vector<char16_t> utf16)
+{
+	std::wstring w(utf16.begin(), utf16.end());
+	return gcnew String(w.c_str());
+}
+
+std::vector<char32_t> InteropService::ToNativeUtf32Chars(String ^ s)
+{
+	auto strIntPtr = Marshal::StringToHGlobalAnsi(s);
+	auto cstr = static_cast<const char*>(strIntPtr.ToPointer());
+
+	std::string utf8(cstr);
+	std::wstring_convert<std::codecvt_utf8_utf16<char32_t>, char32_t> convert;
+	std::u32string utf32 = convert.from_bytes(utf8);
+
+	std::vector<char32_t> writable(utf32.begin(), utf32.end());
+
+	//free resources
+	Marshal::FreeHGlobal(strIntPtr);
+	return writable;
+}
+
+String ^ InteropService::FromNativeUtf32Chars(std::vector<char32_t> utf32)
+{
+	std::wstring w(utf32.begin(), utf32.end());
+	return gcnew String(w.c_str());
+}
+
+
+#endif // _MSC_VER == 1900
+
 
 String ^ InteropService::FromNativeAnsiString(const std::string & s)
 {
 	return gcnew String(s.c_str());
 }
 
-String ^ InteropService::FromNativeUnicodeString(const std::wstring & s)
+String ^ InteropService::FromNativeWideString(const std::wstring & s)
 {
 	return gcnew String(s.c_str());
 }
